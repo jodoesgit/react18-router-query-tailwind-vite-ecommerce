626e86c5d00a56c53f429f26d3eb9160d1a8eb77
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-09-12 22:16:16 -0700

    Established routes

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,57 @@ import {
 	Register,
 	SingleProduct,
 } from "./pages";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <HomeLayout />,
+		errorElement: <Error />,
+		children: [
+			{
+				index: true,
+				element: <Landing />,
+			},
+			{
+				path: "products",
+				element: <Products />,
+			},
+			{
+				path: "products/:id",
+				element: <SingleProduct />,
+			},
+			{
+				path: "cart",
+				element: <Cart />,
+			},
+			{
+				path: "about",
+				element: <About />,
+			},
+			{
+				path: "checkout",
+				element: <Checkout />,
+			},
+			{
+				path: "orders",
+				element: <Orders />,
+			},
+		],
+	},
+	{
+		path: "/login",
+		element: <HomeLayout />,
+		errorElement: <Error />,
+	},
+	{
+		path: "/register",
+		element: <Register />,
+		errorElement: <Error />,
+	},
+]);
 
 const App = () => {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+	return <RouterProvider router={router} />;
 };
 export default App;
