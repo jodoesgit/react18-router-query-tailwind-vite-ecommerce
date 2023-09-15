05c99c1416bcb5e2f31e52d64d390539ab690fa6
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-09-15 00:00:39 -0700

    Setup light/dark mode interface

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
-import { BsCart3, BsMoonFill, BsSunFill } from "react-icons/bs";
+import { BsCart3, BsMoonFill, BsSun, BsSunFill } from "react-icons/bs";
 import { FaBarsStaggered } from "react-icons/fa6";
 import { NavLink } from "react-router-dom";
 import NavLinks from "./NavLinks";
+import { useState } from "react";
 
 const Navbar = () => {
+	const [theme, setTheme] = useState(false);
+	const handleTheme = () => {
+		setTheme(!theme);
+	};
+
 	return (
 		<nav className="bg-base-200">
 			<div className="navbar align-element">
@@ -35,6 +41,13 @@ const Navbar = () => {
 				</div>
 				<div className="navbar-end">
 					{/* THEME SETUP */}
+					<label className="swap swap-rotate">
+						<input type="checkbox" onChange={handleTheme} />
+						{/* sun icon */}
+						<BsSunFill className="swap-on h-4 w-4" />
+						{/* moon icon */}
+						<BsMoonFill className="swap-off h-4 w-4" />
+					</label>
 					<NavLink to="/cart" className="btn btn-ghost btn-circle btn-md ml-4">
 						<div className="indicator">
 							<BsCart3 className="h-6 w-6" />
