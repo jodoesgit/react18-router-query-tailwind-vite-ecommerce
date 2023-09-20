05ecc86ff6cc13733cc82ba6547947ab56ab61ee
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-09-20 14:01:07 -0700

    About page added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,26 @@
 const About = () => {
-	return <h1>About</h1>;
+	return (
+		<>
+			<div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
+				<h1 className="text-4xl font-bold leading-none tracking-tight sm:text-6xl">
+					We love
+				</h1>
+				<div className="stats bg-primary shadow">
+					<div className="stat">
+						<div className="stat-title text-primary-content text-4xl font-bold tracking-widest">
+							COMFY
+						</div>
+					</div>
+				</div>
+			</div>
+			<p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque quae
+				iure cupiditate totam necessitatibus explicabo quibusdam, delectus
+				impedit consequatur, repellat facere nulla, aliquam tempore. Molestiae
+				corporis sunt nulla voluptas eligendi temporibus beatae sequi sed
+				ratione architecto, hic a? Adipisci, neque?
+			</p>
+		</>
+	);
 };
 export default About;
