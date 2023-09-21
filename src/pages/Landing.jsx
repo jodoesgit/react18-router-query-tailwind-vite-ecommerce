63cd99314b8ba3bd7a0168f36aa30b64aa63dade
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-09-21 13:00:52 -0700

    displaying featured products on Landing

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,4 @@
-import { Hero } from "../components";
+import { FeaturedProducts, Hero } from "../components";
 import { customFetch } from "../utils";
 const url = "/products?featured=true";
 
@@ -12,6 +12,7 @@ const Landing = () => {
 	return (
 		<>
 			<Hero />
+			<FeaturedProducts />
 		</>
 	);
 };
