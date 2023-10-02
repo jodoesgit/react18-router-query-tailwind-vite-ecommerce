c89627c23daf1b367638310beb858331ac32e839
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-10-02 14:23:05 -0700

    Setup amount and cart log features

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -14,6 +14,10 @@ const SingleProduct = () => {
 		product.attributes;
 	const dollarsAmount = formatPrice(price);
 	const [productColor, setProductColor] = useState(colors[0]);
+	const [amount, setAmount] = useState(1);
+	const handleAmount = (e) => {
+		setAmount(parseInt(e.target.value));
+	};
 	return (
 		<section>
 			<div className="text-md breadcrumbs">
@@ -26,7 +30,7 @@ const SingleProduct = () => {
 					</li>
 				</ul>
 			</div>
-			{/* PRODUCT */}
+			{/* SINGLE PRODUCT */}
 			<div className="mt-6 grid gap-y-8 lg:grid-cols-2 lg:gap-x-16">
 				{/* IMAGE */}
 				<img
@@ -35,31 +39,60 @@ const SingleProduct = () => {
 					className="w-96 h-96 object-cover rounded-lg lg:w-full"
 				/>
 				{/* PRODUCT */}
-				<h1 className="capitalize text-3xl font-bold">{title}</h1>
-				<h4 className="text-xl text-neutral-content font-bold mt-2">
-					{company}
-				</h4>
-				<p className="mt-3 text-xl">{dollarsAmount}</p>
-				<p className="mt-6 leading-8">{description}</p>
-				{/* COLORS */}
-				<div className="mt-6">
-					<h4 className="text-md font-medium tracking-wider capitalize">
-						colors
+				<div>
+					<h1 className="capitalize text-3xl font-bold">{title}</h1>
+					<h4 className="text-xl text-neutral-content font-bold mt-2">
+						{company}
 					</h4>
-					<div className="mt-2">
-						{colors.map((color) => {
-							return (
-								<button
-									key={color}
-									type="button"
-									className={`badge w-6 h-6 mr-2 ${
-										color === productColor && "border-2 border-secondary"
-									}`}
-									style={{ backgroundColor: color }}
-									onClick={() => setProductColor(color)}
-								></button>
-							);
-						})}
+					<p className="mt-3 text-xl">{dollarsAmount}</p>
+					<p className="mt-6 leading-8">{description}</p>
+					{/* COLORS */}
+					<div className="mt-6">
+						<h4 className="text-md font-medium tracking-wider capitalize">
+							colors
+						</h4>
+						<div className="mt-2">
+							{colors.map((color) => {
+								return (
+									<button
+										key={color}
+										type="button"
+										className={`badge w-6 h-6 mr-2 ${
+											color === productColor && "border-2 border-secondary"
+										}`}
+										style={{ backgroundColor: color }}
+										onClick={() => setProductColor(color)}
+									></button>
+								);
+							})}
+						</div>
+					</div>
+					{/* AMOUNT */}
+					<div className="form-control w-full max-w-xs">
+						<label className="label">
+							<h4 className="text-md font-medium tracking-wider capitalize">
+								amount
+							</h4>
+						</label>
+						<select
+							id="amount"
+							className="select select-secondary select-bordered select-md"
+							value={amount}
+							onChange={handleAmount}
+						>
+							<option value={1}>1</option>
+							<option value={2}>2</option>
+							<option value={3}>3</option>
+						</select>
+					</div>
+					{/* ADD TO CART */}
+					<div className="mt-10">
+						<button
+							className="btn btn-secondary btn-md"
+							onClick={() => console.log("added")}
+						>
+							Add to bag
+						</button>
 					</div>
 				</div>
 			</div>
