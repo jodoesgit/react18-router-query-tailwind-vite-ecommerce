8251658358b125876d16f17907bef433c3407781
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-10-02 13:31:52 -0700

    Rendered data for Single Product

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -13,6 +13,35 @@ const SingleProduct = () => {
 	const { image, title, price, description, colors, company } =
 		product.attributes;
 	const dollarsAmount = formatPrice(price);
-	return <h1>SingleProduct</h1>;
+	return (
+		<section>
+			<div className="text-md breadcrumbs">
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/products">Products</Link>
+					</li>
+				</ul>
+			</div>
+			{/* PRODUCT */}
+			<div className="mt-6 grid gap-y-8 lg:grid-cols-2 lg:gap-x-16">
+				{/* IMAGE */}
+				<img
+					src={image}
+					alt={title}
+					className="w-96 h-96 object-cover rounded-lg lg:w-full"
+				/>
+				{/* PRODUCT */}
+				<h1 className="capitalize text-3xl font-bold">{title}</h1>
+				<h4 className="text-xl text-neutral-content font-bold mt-2">
+					{company}
+				</h4>
+				<p className="mt-3 text-xl">{dollarsAmount}</p>
+				<p className="mt-6 leading-8">{description}</p>
+			</div>
+		</section>
+	);
 };
 export default SingleProduct;
