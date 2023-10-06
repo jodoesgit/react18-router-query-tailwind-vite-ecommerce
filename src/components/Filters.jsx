5394f314d60fb9d7be626d7efae9ec4bdca8c5f0
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-10-06 14:25:27 -0700

    Added company dropdown selection

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,7 +1,9 @@
 import { Form, useLoaderData, Link } from 'react-router-dom';
 import FormInput from './FormInput';
+import FormSelect from './FormSelect';
 
 const Filters = () => {
+	const { meta } = useLoaderData();
 	return (
 		<Form className="bg-base-200 round-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
 			{/* SEARCH */}
@@ -11,6 +13,15 @@ const Filters = () => {
 				name="search"
 				size="input-sm"
 			/>
+			{/* CATEGORIES */}
+			{/* COMPANIES */}
+			<FormSelect
+				label="select company"
+				name="company"
+				list={meta.companies}
+				size="select-sm"
+			/>
+			{/* ORDER */}
 			{/* BUTTONS */}
 			<button type="submit" className="btn btn-primary btn-sm">
 				search
