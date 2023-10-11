3e52bfd037cbd140dd5941de2fff63f11a396521
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-10-10 19:29:42 -0700

    Connected Pagination to API

--- src/components/PaginationContainer.jsx
+++ src/components/PaginationContainer.jsx
@@ -6,8 +6,12 @@ const PaginationContainer = () => {
 	const pages = Array.from({ length: pageCount }, (_, index) => {
 		return index + 1;
 	});
+	const { search, pathname } = useLocation();
+	const navigate = useNavigate();
 	const handlePageChange = (pageNumber) => {
-		console.log(pageNumber);
+		const searchParams = new URLSearchParams(search);
+		searchParams.set('page', pageNumber);
+		navigate(`${pathname}?${searchParams.toString()}`);
 	};
 
 	if (pageCount < 2) return null;
@@ -16,7 +20,10 @@ const PaginationContainer = () => {
 			<button
 				className="btn btn-xs sm:btn-md join-item"
 				onClick={() => {
-					handlePageChange('prev');
+					let prevPage = page - 1;
+					if (prevPage < 1) prevPage = pageCount;
+
+					handlePageChange(prevPage);
 				}}
 			>
 				Prev
@@ -37,7 +44,9 @@ const PaginationContainer = () => {
 			<button
 				className="btn btn-xs sm:btn-md join-item"
 				onClick={() => {
-					handlePageChange('next');
+					let nextPage = page + 1;
+					if (nextPage > pageCount) nextPage = 1;
+					handlePageChange(nextPage);
 				}}
 			>
 				Next
