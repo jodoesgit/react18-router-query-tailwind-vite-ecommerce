265f13db25ad6aaff12581a91025fb38412f8d2a
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-10-27 12:10:18 -0700

    Connected registration to API

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,23 @@
 import { FormInput, SubmitBtn } from '../components';
-import { Form, Link } from 'react-router-dom';
+import { Form, Link, redirect } from 'react-router-dom';
+import { customFetch } from '../utils';
+import { toast } from 'react-toastify';
 
-export const action = async () => {
-	return null;
+export const action = async ({ request }) => {
+	const formData = await request.formData();
+	const data = Object.fromEntries(formData);
+
+	try {
+		const response = await customFetch.post('/auth/local/register', data);
+		toast.success('account created successfully');
+		return redirect('/login');
+	} catch (error) {
+		const errorMessage =
+			error?.response?.data?.error?.message ||
+			'please double check your credentials';
+		toast.error(errorMessage);
+		return null;
+	}
 };
 
 const Register = () => {
@@ -14,7 +29,7 @@ const Register = () => {
 			>
 				<h4 className="text-center text-3xl font-bold">Register</h4>
 				<FormInput type="text" label="username" name="username" />
-				<FormInput type="email" label="useremail" name="useremail" />
+				<FormInput type="email" label="email" name="email" />
 				<FormInput type="password" label="password" name="password" />
 				<div className="mt-4">
 					<SubmitBtn text="register" />
