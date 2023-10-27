0a2dbb10a841647859aa4db820a60a54dfb1f466
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-10-27 13:00:37 -0700

    Fixed Hero bug/Added user to userSlice

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,9 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-import hero1 from "../assets/hero1.webp";
-import hero2 from "../assets/hero2.webp";
-import hero3 from "../assets/hero3.webp";
-import hero4 from "../assets/hero4.webp";
+import hero1 from '../assets/hero1.webp';
+import hero2 from '../assets/hero2.webp';
+import hero3 from '../assets/hero3.webp';
+import hero4 from '../assets/hero4.webp';
 
 const carouselImages = [hero1, hero2, hero3, hero4];
 
@@ -28,7 +28,7 @@ const Hero = () => {
 			<div className="hidden h-[28rem] lg:carousel carousel-center p-4 space-x-4 bg-neutral rounded-box">
 				{carouselImages.map((image) => {
 					return (
-						<div className="carousel-item">
+						<div key={image} className="carousel-item">
 							<img
 								src={image}
 								className="rounded-box h-full w-80 object-cover"
