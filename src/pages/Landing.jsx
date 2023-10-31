60d5f83a8e5fce7b650176e28d88fed8d0c35733
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-10-30 20:38:54 -0700

    Added QueryClient

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,19 +1,19 @@
-import { FeaturedProducts, Hero } from "../components";
-import { customFetch } from "../utils";
-const url = "/products?featured=true";
+import { FeaturedProducts, Hero } from '../components';
+import { customFetch } from '../utils';
+const url = '/products?featured=true';
 
-export const loader = async () => {
-	const response = await customFetch(url);
-	const products = response.data.data;
-	return { products };
+export const loader = (queryClient) => async () => {
+  const response = await customFetch(url);
+  const products = response.data.data;
+  return { products };
 };
 
 const Landing = () => {
-	return (
-		<>
-			<Hero />
-			<FeaturedProducts />
-		</>
-	);
+  return (
+    <>
+      <Hero />
+      <FeaturedProducts />
+    </>
+  );
 };
 export default Landing;
